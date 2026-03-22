8a2aa1fb8ff6063a8283709e976f508492d41b97
HungHuaiYang <yangsam50601@gmail.com>
2026-03-22 21:50:01 +0800

    完成 A0014: 二進位與十六進位

---
+++ Csharp autocodegrader/第一次作業/A0014.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.第一次作業
+{
+    internal class A0014
+    {
+        static void Main(string[] args)
+        {
+            // 系統可能會預先放入其他的數值，這裡保留你的範例作為測試
+            int N = 0x212 + 22011;
+            int M = 0b101011010110;
+
+            // 在此加入程式碼並依要求輸出
+            Console.WriteLine($"N的十進位表示是:{N}");
+            Console.WriteLine($"M的十進位表示是:{M}");
+
+            Console.ReadLine();
+        }
+    }
+}
