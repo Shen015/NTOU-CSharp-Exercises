7128172dfb889fa90531762e251075b58bcc7eb3
HungHuaiYang <yangsam50601@gmail.com>
2026-03-22 17:43:45 +0800

    A0000 - hello world 測試

---
+++ Csharp autocodegrader/第一次作業/A0000.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.第一次作業
+{
+    internal class A0000
+    {
+        static void Main(string[] args)
+        {
+            // entering codes here
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
