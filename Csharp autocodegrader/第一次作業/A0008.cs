5f19d58f1e871079e6f2cfbacbd5219a3e67a5ee
HungHuaiYang <yangsam50601@gmail.com>
2026-03-22 21:33:17 +0800

    完成 A0008:跳脫字元練習

---
+++ Csharp autocodegrader/第一次作業/A0008.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.第一次作業
+{
+    internal class A0008
+    {
+        static void Main(string[] args)
+        {
+            // your codes start here
+            // 記得將 kiki 和 1234@mail.ntou.edu.tw 換成你自己的資訊喔！
+            string userName = "Shen015";
+            string email = "01489027@mail.ntou.edu.tw";
+
+            // 解析：
+            // \"   用來輸出雙引號 (")
+            // \\\\ 用來輸出兩個反斜線 (\\) ，因為一個 \ 需要用 \\ 來跳脫
+            // \r\n 用來執行 Windows 系統的標準換行
+            Console.WriteLine("\"{0}\\\\Username\"\r\n\"{1}\\\\Email\"", userName, email);
+
+            Console.ReadLine();
+        }
+    }
+}
