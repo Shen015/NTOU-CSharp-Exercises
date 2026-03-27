224d242e5dd15b4777ae2b5418070587de9526d9
HungHuaiYang <yangsam50601@gmail.com>
2026-03-27 12:25:35 +0800

    完成 A0001: 拷貝貓

---
+++ Csharp autocodegrader/A系列其他/A0001.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.A系列其他
+{
+    internal class A0001
+    {   
+        static void Main(string[] args)
+        {
+            // entering codes here
+            //輸出與輸入都不需提示
+            var i = Console.ReadLine();
+            Console.WriteLine(i);
+        }
+    }
+}
