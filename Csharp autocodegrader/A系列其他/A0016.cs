acdc08e78f403ab7eaaed3c47036a86ac3e59110
HungHuaiYang <yangsam50601@gmail.com>
2026-03-27 16:55:44 +0800

    完成 A0016: Cast轉型

---
+++ Csharp autocodegrader/A系列其他/A0016.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.A系列其他
+{
+    internal class A0016
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("請入正整數字N=");
+            string sN = Console.ReadLine();
+            int N = Convert.ToInt32(sN);
+
+            Console.WriteLine("請入正整數字M=");
+            string sM = Console.ReadLine();
+            int M = Convert.ToInt32(sM);
+
+            // 修正 1：在 N 前面加上 (float) 進行強制轉型，讓系統執行浮點數除法
+            float f = (float)N / M;
+
+            // 修正 2：利用之前學過的字串內插 ($"")，並加上 :F4 來四捨五入到小數點後第四位
+            Console.WriteLine($"{N}/{M}={f:F4}");
+
+            // 以下勿動
+            Console.WriteLine($"Typeof f is {f.GetType().ToString()}.");
+            Console.ReadLine();
+        }
+    }
+}
