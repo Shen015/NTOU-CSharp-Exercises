25191ed9c93e0fdf7948e5fec3fcae744ecb5f08
HungHuaiYang <yangsam50601@gmail.com>
2026-03-27 17:22:40 +0800

    完成 A0013: Hello! My username is...

---
+++ Csharp autocodegrader/A系列其他/A0013.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.A系列其他
+{
+    internal class A0013
+    {
+        static void Main(string[] args)
+        {
+            // 直接輸出一整段字串，請將 Shen015 替換成你的真實使用者名稱（如果系統有要求的話）
+            Console.WriteLine("Hello! My username is Shen015.");
+        }
+    }
+}
