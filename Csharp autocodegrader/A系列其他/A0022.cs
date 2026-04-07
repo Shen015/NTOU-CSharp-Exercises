5d0a63c26bc2c2707b92a697d45450c8879d86db
HungHuaiYang <yangsam50601@gmail.com>
2026-04-07 20:24:35 +0800

    完成 A0022: 三角文字排列

---
+++ Csharp autocodegrader/A系列其他/A0022.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_autocodegrader.A系列其他
+{
+    internal class A0022
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string:");
+            string s = Console.ReadLine();
+
+            // 1. 處理使用者輸入的字串
+            // 迴圈從長度 1 開始，直到字串的總長度
+            for (int i = 1; i <= s.Length; i++)
+            {
+                // Substring(0, i) 代表從索引 0 開始，擷取長度為 i 的字串
+                Console.WriteLine(s.Substring(0, i));
+            }
+
+            // 2. 設定你的 username (如果你的 username 不同，可以直接修改這裡)
+            string username = "Shen015";
+
+            // 3. 如法炮製，處理 username
+            for (int i = 1; i <= username.Length; i++)
+            {
+                Console.WriteLine(username.Substring(0, i));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
